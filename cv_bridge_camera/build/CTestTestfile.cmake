94a92525eff98dd0aa68e8062e5684eb40df657c
June <czt8kd@virginia.edu>
2025-02-19 19:29:31 -0500

    Create cv_bridge_camera workspace

---
+++ cv_bridge_camera/build/CTestTestfile.cmake
@@ -0,0 +1,7 @@
+# CMake generated Testfile for 
+# Source directory: /home/june/cv_bridge_camera/src
+# Build directory: /home/june/cv_bridge_camera/build
+# 
+# This file includes the relevant testing commands required for 
+# testing this directory and lists subdirectories to be tested as well.
+subdirs("gtest")
