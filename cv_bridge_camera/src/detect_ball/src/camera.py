2db806d3d0369eb25342e7613dbbd2d183fb8fd1
June <czt8kd@virginia.edu>
2025-02-26 15:09:39 -0500

    Finish Circle Detection
    Circle detection by comparing area of enclosing circle with area of contour

--- cv_bridge_camera/src/detect_ball/src/camera.py
+++ cv_bridge_camera/src/detect_ball/src/camera.py
@@ -18,14 +18,14 @@ class image_converter:
     self.image_pub = rospy.Publisher("image_topic_2",Image)
     self.center_pub = rospy.Publisher("centers",String)
     self.bridge = CvBridge()
-    self.image_sub = rospy.Subscriber("/camera/color/image_raw",Image,self.callback)
+    self.image_sub = rospy.Subscriber("/usb_cam/image_raw",Image,self.callback)
 
   def callback(self,data):
     try:
       cv_image = self.bridge.imgmsg_to_cv2(data, "bgr8")
     except CvBridgeError as e:
       print(e)
-
+    pi = 3.14159
     (rows,cols,channels) = cv_image.shape
     if cols > 60 and rows > 60 :
       cv2.circle(cv_image, (50,50), 10, 255)
@@ -45,16 +45,28 @@ class image_converter:
     centers = [None]*len(contours)
     radius = [None]*len(contours)
     contours_poly = [None]*len(contours)
+    contours_circlescore = [None]*len(contours)
+    circle_centers = []
+    circle_radii = []
+    circle_circlescore = []
+    circle_contours = []
     for i, c in enumerate(contours):
         contours_poly[i] = cv2.approxPolyDP(c, 3, True)
         centers[i], radius[i] = cv2.minEnclosingCircle(contours_poly[i])
-        
-    max_rad = max(radius)
-    max_index = radius.index(max_rad)
-    max_center = centers[max_index]
-    cv2.circle(cv_image, (int(max_center[0]), int(max_center[1])), int(max_rad), 255)
-    cv2.circle(cv_image, (int(max_center[0]), int(max_center[1])), int(5), 50)
-    self.center_pub.publish("Center of Largest Contour: ("+ str(max_center[0]) +", " +str(max_center[1])+")")
+        contours_circlescore[i] = cv2.contourArea(c)/(pi*radius[i]**2)
+        if contours_circlescore[i] > 0.5 and radius[i] > 5:
+            circle_centers.append(centers[i])
+            circle_radii.append(radius[i])
+            circle_circlescore.append(contours_circlescore[i])
+            circle_contours.append(c)
+    if(circle_radii != []):
+        max_rad = max(circle_radii)
+        max_index = circle_radii.index(max_rad)
+        max_center = circle_centers[max_index]
+        max_circlescore = circle_circlescore[max_index]
+        cv2.circle(cv_image, (int(max_center[0]), int(max_center[1])), int(max_rad), 255)
+        cv2.circle(cv_image, (int(max_center[0]), int(max_center[1])), int(5), 50)
+        self.center_pub.publish("Center of Largest Circular Contour: ("+ str(max_center[0]) +", " +str(max_center[1])+") \n Circle Score: " + str(max_circlescore))
     """detected_circles = cv2.HoughCircles(mask,  
                 cv2.HOUGH_GRADIENT, 1, 2, param1 = 50, 
             param2 = 30, minRadius = 1, maxRadius = 40)
@@ -70,7 +82,7 @@ class image_converter:
     
             # Draw a small circle (of radius 1) to show the center. 
             cv2.circle(mask, (a, b), 1, (0, 0, 255), 3) """
-    cv2.drawContours(cv_image, contours, -1, (0, 255, 0), 3)
+    cv2.drawContours(cv_image, circle_contours, -1, (0, 255, 0), 3)
     cv2.imshow("Mask window", mask)
     cv2.imshow("Image window", cv_image)
     cv2.waitKey(3)
