6864c43767c3fa119343cc9d51d9ee662b1c4322
june-wiles <czt8kd@virginia.edu>
2025-03-05 13:25:53 -0500

    Update distance_sensor.py

--- cv_bridge_camera/src/detect_ball/src/distance_sensor.py
+++ cv_bridge_camera/src/detect_ball/src/distance_sensor.py
@@ -1,9 +1,19 @@
-#!/usr/bin/env python
+#!/usr/bin/env python3
+from __future__ import print_function
 import rospy
 from std_msgs.msg import Float32
-from publisher_controlvalue import ControlValuePub
 import numpy as np
 
+
+import roslib
+roslib.load_manifest('detect_ball')
+import sys
+import rospy
+import cv2
+from std_msgs.msg import String
+from sensor_msgs.msg import Image
+from cv_bridge import CvBridge, CvBridgeError
+import numpy as np
 class MySubscriber(object):
 
     def __init__(self):
@@ -13,20 +23,25 @@ class MySubscriber(object):
 
 
         rospy.Subscriber('/centers', String, self.centers_callback)
-        rospy.Subscriber('/FILLER', Float32, self.distance_callback)
+        rospy.Subscriber('/d400/aligned_depth_to_color/image_raw', Image, self.distance_callback)
 
 
     def centers_callback(self,msg):
         self.centers = msg.data
 
     def distance_callback(self,msg):
+        cv_bridge = CvBridge()
+        try:
+            depth_img = cv_bridge.imgmsg_to_cv2(msg, desired_encoding='passthrough')
+        except CvBridgeError as e:
+            print(e)
+        depth_array = np.array(depth_img, dtype = np.float32)
         detected_objects = self.centers
-        print(msg.shape)
         if detected_objects:
-            for obj in detected_objects:
-                world_coords = self.transform_to_world(obj)
-                probability = self.compute_probability(world_coords[0])
-                self.publish_marker(world_coords, probability)
+            
+            world_coords = self.transform_to_world(obj)
+            probability = self.compute_probability(world_coords[0])
+            self.publish_marker(world_coords, probability)
     def transform_to_world(self, detection):
         """ Transforms detected coordinates to world frame using TF2. """
         try:
